0687c62d1f5cd176604ebf6bcbbba5bbdb35960a
Guilherme Marconi <guiilhermemarconi@gmail.com>
2017-05-05 21:04:21 -0300

    atualiza clientes

--- gtx.js
+++ gtx.js
@@ -14,8 +14,10 @@ new Vivus('logoLines', {
 });
 
 // Iniciate HideSeek plugin to search clients
-$('.searchinput').addClass('is-visible')
+$('.searchinput')
+  .addClass('is-visible')
   .hideseek({
+    attribute: 'data-keywords',
     highlight: true,
     ignore_accents: true,
   });
@@ -27,35 +29,45 @@ fetch('clients.json')
   .then(data => {
     const { clients } = data;
     clients.map((client, i) => {
-      const { name, id } = client;
+      const { name, id, keywords } = client;
       const delay = i * 75;
       const html = `
-          <div class="client" style="transition-delay: ${delay}ms">
-            <h3 class="client__name">${name}</h3>
-            <ul class="client__links">
-              <li class="client__item">
-                <a class="client__link" href="http://${id}.vtexcommercestable.com.br">
-                  Stable
-                </a>
-              </li>
-              <li class="client__item">
-                <a class="client__link" href="http://${id}.vtexcommercestable.com.br/admin">
-                  Admin
-                </a>
-              </li>
-              <li class="client__item">
-                <a class="client__link" href="http://${id}.vtexcrm.com.br">
-                  CRM
-                </a>
-              </li>
-              <li class="client__item">
-                <a class="client__link" href="http://${id}.ds.vtexcrm.com.br">
-                  Master Data
-                </a>
-              </li>
-            </ul>
-          </div>
-        `;
+        <div class="client" data-keywords="${keywords}" style="transition-delay: ${delay}ms">
+          <h3 class="client__name">${name}</h3>
+          <ul class="client__links">
+            <li class="client__item">
+              <a class="client__link" href="http://${id}.vtexcommercestable.com.br">
+                Stable
+              </a>
+            </li>
+            <li class="client__item">
+              <a class="client__link" href="http://${id}.vtexcommercestable.com.br/admin">
+                Admin
+              </a>
+            </li>
+            <li class="client__item">
+              <a class="client__link" href="http://${id}.vtexcommercestable.com.br/admin/a">
+                CMS
+              </a>
+            </li>
+            <li class="client__item">
+              <a class="client__link" href="http://${id}.vtexcommercestable.com.br/admin/portal">
+                Portal
+              </a>
+            </li>
+            <li class="client__item">
+              <a class="client__link" href="http://${id}.vtexcrm.com.br">
+                CRM
+              </a>
+            </li>
+            <li class="client__item">
+              <a class="client__link" href="http://${id}.ds.vtexcrm.com.br">
+                Master Data
+              </a>
+            </li>
+          </ul>
+        </div>
+      `;
       clientsList.append(html);
     });
   });
